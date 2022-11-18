a906f7c2608567d624463496832188122838b3e5
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-11-18 14:52:42 -0600

    Initial Commit

---
+++ TestingInteropAllSwift/TestingInteropAllSwift.h
@@ -0,0 +1,18 @@
+//
+//  TestingInteropAllSwift.h
+//  TestingInteropAllSwift
+//
+//  Created by Jax DesMarais-Leder on 11/18/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestingInteropAllSwift.
+FOUNDATION_EXPORT double TestingInteropAllSwiftVersionNumber;
+
+//! Project version string for TestingInteropAllSwift.
+FOUNDATION_EXPORT const unsigned char TestingInteropAllSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingInteropAllSwift/PublicHeader.h>
+
+
